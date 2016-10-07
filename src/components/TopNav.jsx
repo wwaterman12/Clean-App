f0bbe67d9e6036adce85d0e8e52d0e72b81bbd5d
Erik Jönsson <efjonsson@gmail.com>
2016-10-07 15:02:00 -0400

    center logotype, position logo-icon and logotype

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -4,7 +4,8 @@ class TopNav extends Component {
   render() {
     return (
       <div className="top-nav">
-        <img className="logo-image" src="./images/cleanapp_logo_icon.png" alt="" />
+        <img className="logo-icon" src="./images/cleanapp_logo_icon.png" alt="Cleanapp logo" />
+        <img className="logotype" src="./images/cleanapp_logotype.png" alt="Cleanapp" />
       </div>
     );
   }
