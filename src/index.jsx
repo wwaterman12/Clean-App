c749c7bdc7756da2b562dda56eb8321d5bbac5c2
Erik Jönsson <efjonsson@gmail.com>
2016-10-07 14:06:45 -0400

    create main app route, create dummy app component

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Routes from './config/Routes.jsx';
 
-class App extends React.Component {
-  render () {
-    return (
-      <h1>YAY</h1>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<Routes />, document.querySelector('#root'));
