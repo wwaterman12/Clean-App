cd9d577a7b006f3998ff2f7be11bc6eccbed523e
Erik Jönsson <efjonsson@gmail.com>
2016-10-07 14:49:30 -0400

    rename duck logo file

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -4,7 +4,7 @@ class TopNav extends Component {
   render() {
     return (
       <div className="top-nav">
-        <img className="logo-image" src="./images/logo-duck.png" alt="" />
+        <img className="logo-image" src="./images/cleanapp_logo_duck.png" alt="" />
       </div>
     );
   }
