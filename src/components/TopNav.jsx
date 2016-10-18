23c7ed59259622e8df65a24a7722d6024f982ef9
Erik Jönsson <efjonsson@gmail.com>
2016-10-17 22:40:47 -0400

    fix nav bar height and positioning

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -6,12 +6,23 @@ class TopNav extends Component {
     super();
     this.state = {
       menuOpen: false,
+      navHeight: '0px',
     };
     this.toggleClass = this.toggleClass.bind(this);
   }
   componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+    this.handleNavResize();
     this.menuIcon();
     this.menuItemClass();
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleNavResize);
+  }
+  handleNavResize() {
+    const bodyHeight = document.body.offsetHeight;
+    const navHeight = `${bodyHeight * 0.1}px`;
+    this.setState({ navHeight });
   }
   menuIcon() {
     return this.state.menuOpen === false ? '☰' : 'x';
@@ -20,31 +31,25 @@ class TopNav extends Component {
     return this.state.menuOpen === false ? 'nav-item nav-item--closed' : 'nav-item nav-item--open';
   }
   toggleClass() {
-    this.state.menuOpen === false
-      ? this.setState({ menuOpen: true })
-      : this.setState({ menuOpen: false });
+    this.state.menuOpen === false ? this.setState({ menuOpen: true }) : this.setState({ menuOpen: false });
   }
   render() {
     return (
       <div className="menu clearfix">
-        <div className="top-nav clearfix">
-          <Link to="/">
-            <img
-              className="logo-icon"
-              src="./images/cleanapp_logo_icon.png"
-              alt="Cleanapp logo"
-            />
+        <div className="top-nav clearfix" style={{ height: this.state.navHeight }} >
+          <Link to="/" className="logo-icon">
+            <img src="./images/cleanapp_logo_icon.png" alt="Cleanapp logo" />
           </Link>
-          <Link to="/">
-            <img className="logotype" src="./images/cleanapp_logotype.png" alt="Cleanapp" />
+          <Link to="/" className="logotype">
+            <img src="./images/cleanapp_logotype.png" alt="Cleanapp" />
           </Link>
           <button className="hamburger" onClick={this.toggleClass}>{this.menuIcon()}</button>
         </div>
         <Link to="/" className={this.menuItemClass()} onClick={this.toggleClass} >
-        What is Cleanapp?
+          What is Cleanapp?
         </Link>
         <Link to="/" className={this.menuItemClass()} onClick={this.toggleClass} >
-        How We Keep the Prices Low
+          How We Keep the Prices Low
         </Link>
         <Link to="/login" className={`${this.menuItemClass()} nav-item--bottom`} onClick={this.toggleClass} >
         Log In
