bed4fa49ffacf0c3f657c93c4653fe5772f9d375
Erik Jönsson <efjonsson@gmail.com>
2016-10-17 23:20:01 -0400

    adjust finish screen for new nav

--- src/components/finish.jsx
+++ src/components/finish.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router';
 
 const Finish = () => (
   <div className="main-child finish-container">
-    <p className="thank-you">Thank You for your order!</p>
-    <img className="checkmark" src="./images/cleanapp_checkmark.png" alt="checkmark" />
-    <p className="order-number">order no:15687923</p>
-    <p className="confirm-email">We&#39;ve sent you a confirmation<br />to your e-mail.</p>
+    <div className="finish-content">
+      <h3 className="finish_thank-you">Thank you for your order!</h3>
+      <img className="finish_checkmark" src="./images/cleanapp_checkmark.png" alt="checkmark" />
+      <p className="finish_order-number">order no: <span>15687923</span></p>
+      <p className="finish_confirm-email">We&#39;ve sent you a confirmation to your e-mail.</p>
+    </div>
     <Link className="bottom-button" to="/">Return to home</Link>
   </div>
 );
