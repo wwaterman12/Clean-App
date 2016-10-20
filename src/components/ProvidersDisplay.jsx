24252fad50342886bac4d3b06dc3a5e5da759bc3
Erik Jönsson <efjonsson@gmail.com>
2016-10-20 17:58:50 -0400

    style fixes in provider details

--- src/components/ProvidersDisplay.jsx
+++ src/components/ProvidersDisplay.jsx
@@ -8,7 +8,7 @@ const propTypes = {
 };
 
 class ProvidersDisplay extends React.Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       currentWeek: null,
@@ -16,98 +16,56 @@ class ProvidersDisplay extends React.Component {
         {
           name: 'OsoujiMasters',
           prices: {
-            AC1: 6500,
-            AC2: 7500,
-            Range: 8000,
-            Bathroom: 7000,
-            Toilet: 5000,
+            airCondition1: 6500,
+            airCondition2: 7500,
+            rangeHood: 8000,
+            bathroom: 7000,
+            toilet: 5000,
           },
           availabilities: [
-            {
-              start: 1000,
-              end: 1200,
-            },
-            {
-              start: 1600,
-              end: 1800,
-            },
-            {},
-            {},
-            {
-              start: 1100,
-              end: 1300,
-            },
-            {},
-            {
-              start: 1000,
-              end: 1400,
-            },
+            { start: 1000, end: 1200 },
+            { start: 1600, end: 1800 },
+            { start: 1100, end: 1300 },
+            { start: 1000, end: 1400 },
           ],
         },
         {
           name: 'CleaningDucks',
           prices: {
-            AC1: 7000,
-            AC2: 8000,
-            Range: 7900,
-            Bathroom: 8000,
-            Toilet: 5500,
+            airCondition1: 7000,
+            airCondition2: 8000,
+            rangeHood: 7900,
+            bathroom: 8000,
+            toilet: 5500,
           },
           availabilities: [
-            {},
-            {
-              start: 1300,
-              end: 1500,
-            },
-            {},
-            {},
-            {
-              start: 1000,
-              end: 1300,
-            },
-            {},
-            {},
-          ]
+            { start: 1300, end: 1500 },
+            { start: 1000, end: 1300 },
+          ],
         },
         {
           name: 'SuperCleaners',
           prices: {
-            AC1: 8000,
-            AC2: 10000,
-            Range: 5000,
-            Bathroom: 8000,
-            Toilet: 5000,
+            airCondition1: 8000,
+            airCondition2: 10000,
+            rangeHood: 5000,
+            bathroom: 8000,
+            toilet: 5000,
           },
           availabilities: [
-            {},
-            {
-              start: 1200,
-              end: 1400,
-            },
-            {
-              start: 1300,
-              end: 1500,
-            },
-            {},
-            {
-              start: 1000,
-              end: 1400,
-            },
-            {},
-            {},
+            { start: 1200, end: 1400 },
+            { start: 1300, end: 1500 },
+            { start: 1000, end: 1400 },
           ],
         },
-      ]
+      ],
     };
     this.getDayAndMonth = this.getDayAndMonth.bind(this);
     this.incrementWeek = this.incrementWeek.bind(this);
     this.nextWeek = this.nextWeek.bind(this);
     this.lastWeek = this.lastWeek.bind(this);
   }
-  associateDates () {
-
-  }
-  componentWillMount () {
+  componentWillMount() {
     const today = new Date();
     const sevenDays = new Date(Date.now() + 604800000);
     this.setState({
@@ -117,7 +75,7 @@ class ProvidersDisplay extends React.Component {
       },
     });
   }
-  getDayAndMonth () {
+  getDayAndMonth() {
     const today = this.state.currentWeek.startDate;
     const nextWeek = this.state.currentWeek.endDate;
     const weekData = {
@@ -128,16 +86,18 @@ class ProvidersDisplay extends React.Component {
     };
     return weekData;
   }
-  incrementWeek (direction) {
+  // associateDates() {
+  //
+  // }
+  incrementWeek(direction) {
     const startDate = this.state.currentWeek.startDate;
     const endDate = this.state.currentWeek.endDate;
     let newStart;
     let newEnd;
     if (direction > 0) {
-      newStart = new Date(startDate.valueOf() + 1000*60*60*24*8);
-      newEnd = new Date(endDate.valueOf() + 1000*60*60*24*8);
-    }
-    else if (direction < 0) {
+      newStart = new Date(startDate.valueOf() + (1000 * 60 * 60 * 24 * 8));
+      newEnd = new Date(endDate.valueOf() + (1000 * 60 * 60 * 24 * 8));
+    } else if (direction < 0) {
       newStart = new Date(startDate - 691200000);
       newEnd = new Date(endDate - 691200000);
     }
@@ -145,46 +105,49 @@ class ProvidersDisplay extends React.Component {
       currentWeek: {
         startDate: newStart,
         endDate: newEnd,
-      }
+      },
     });
   }
-  nextWeek () {
+  nextWeek() {
     this.incrementWeek(1);
   }
-  lastWeek () {
+  lastWeek() {
     this.incrementWeek(-1);
   }
-  render () {
+  render() {
     const week = this.getDayAndMonth(this.state.currentWeek);
     return (
       <div>
-        <div id='week-bar'>
-          <span id="left-week-button" className='week-button' onClick={this.lastWeek}>T</span>
-          <span id='dates-display'>
+        <div id="week-bar">
+          <button
+            id="left-week-button"
+            className="week-button"
+            onClick={this.lastWeek}
+          >◀</button>
+          <span id="dates-display">
             {`${week.startMonth}月${week.startDay}日~${week.endMonth}月${week.endDay}日`}
           </span>
-          <span id="right-week-button" className='week-button' onClick={this.nextWeek}>T</span>
+          <button
+            id="right-week-button"
+            className="week-button"
+            onClick={this.nextWeek}
+          >▶</button>
         </div>
-        <div id='filter-options'>
+        <div id="filter-options">
           <div>Advanced time search</div>
-          <div>Sorted by: lowest price</div>
+          <div>Sorted by: Lowest price</div>
         </div>
         <div>
-          {
-            this.state.providers.map((vendor) => {
-              return vendor.availabilities.map((block) => {
-                return (
-                  <Link to='vendor-details'>
-                    <ProviderOption prices={vendor.prices}
-                                    selectedServices={this.props.selection}
-                                    availability={block}
-                                    selectBlock={this.props.setVendorAndTime}
-                                />
-                  </Link>
-                );
-              });
-            })
-          }
+          { this.state.providers.map(vendor => vendor.availabilities.map(block => (
+            <Link to="vendor-details">
+              <ProviderOption
+                prices={vendor.prices}
+                selectedServices={this.props.selection}
+                availability={block}
+                selectBlock={this.props.setVendorAndTime}
+              />
+            </Link>
+          ))) }
         </div>
       </div>
     );
