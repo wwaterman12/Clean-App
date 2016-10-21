61cc2e516ee3a83dad80c86780e47970b2a23abd
Erik Jönsson <efjonsson@gmail.com>
2016-10-20 22:29:29 -0400

    provider details page styling

--- src/components/providers/ProviderDetails.jsx
+++ src/components/providers/ProviderDetails.jsx
@@ -12,13 +12,20 @@ class ProviderDetails extends Component {
   render() {
     return (
       <div className="provider-details-container">
-        <h2 className="provider-details_header">Provider details:</h2>
-        <h1 className="provider-name">{this.props.selectedProvider.name}</h1>
-        <h3 className="provider-rating">(4.2/5) *****</h3>
+        <div className="provider-details_header">
+          <Link to="/providers" className="back-button" >◀</Link>
+          <h2 className="provider-details-title">Provider details:</h2>
+          <h1 className="provider-name">{this.props.selectedProvider.name}</h1>
+          <h3 className="provider-rating">(4.2/5) *****</h3>
+        </div>
         <div className="provider-image"></div>
-        <div className="provider-price">{this.props.totalPrice}</div>
-        <Link to={`#/providers/${this.props.selectedProvider.id}`}>User comments &gt;</Link>
-        <Link>Company summary &lt;</Link>
+        <div className="provider-price">
+          <span className="list-price">¥ 13,800</span> ¥ {this.props.totalPrice}
+        </div>
+        <div className="best-price-tag">Best price!</div>
+        <Link className="detail-link">User comments <span>&gt;</span></Link>
+        <Link className="detail-link">Company summary <span>&gt;</span></Link>
+        <Link to="confirm-order" className="bottom-button">Proceed to Checkout</Link>
       </div>
     );
   }
