a8be3e32035995955b4257f4f1a17b93311d189a
Erik Jönsson <efjonsson@gmail.com>
2016-10-20 21:19:32 -0400

    create bottom button global style

--- src/components/providers/ProviderDetails.jsx
+++ src/components/providers/ProviderDetails.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 const propTypes = {
   params: React.PropTypes.object,
+  selectedProvider: React.PropTypes.object,
+  totalPrice: React.PropTypes.number,
 };
 
 
 class ProviderDetails extends Component {
   render() {
     return (
-      <h1>Provider {this.props.params.id}</h1>
+      <div className="provider-details-container">
+        <h2 className="provider-details_header">Provider details:</h2>
+        <h1 className="provider-name">{this.props.selectedProvider.name}</h1>
+        <h3 className="provider-rating">(4.2/5) *****</h3>
+        <div className="provider-image"></div>
+        <div className="provider-price">{this.props.totalPrice}</div>
+        <Link to={`#/providers/${this.props.selectedProvider.id}`}>User comments &gt;</Link>
+        <Link>Company summary &lt;</Link>
+      </div>
     );
   }
 }
