fb5e0f29a65bf2e4268190c48c1fc6173db99545
Wesley Waterman <wwaterman12@gmail.com>
2016-10-21 20:01:19 -0400

    add cc images to payments, modify payments page styles

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -26,14 +26,20 @@ class Payment extends React.Component {
       <div>
         <div className="heading">Fill in your payment information</div>
         <div className="cc-icons">
+          <img className="cc-image" src="./images/1477108764_Visa-Curved.png" alt="visa" height="30" width="50" />
+          <img className="cc-image" src="./images/1477108800_American-Express-Curved.png" alt="visa" height="30" width="50" />
+          <img className="cc-image" src="./images/1477108823_Mastercard-Curved.png" alt="visa" height="30" width="50" />
+          <img className="cc-image" src="./images/1477108833_Discover-Curved.png" alt="visa" height="30" width="50" />
         </div>
         <div className="cc-form">
-           <input type="text" className="block-input" onChange={this.handleChange} name="ccNumber" placeholder="Credit Card Number" />
-           <input type="text" className="in-line-input" onChange={this.handleChange} name="expiration" placeholder="Expiration date MM/YY" />
-           <input type="text" className="in-line-input" onChange={this.handleChange} name="CVC" placeholder="CVC" />
+           <input type="text" className="cc-input" onChange={this.handleChange} name="ccNumber" placeholder="Credit Card Number" />
+           <input type="text" className="expiration-input" onChange={this.handleChange} name="expiration" placeholder="Expiration date MM/YY" />
+           <input type="text" className="cvc-input" onChange={this.handleChange} name="CVC" placeholder="CVC" />
         </div>
-        <p></p>
-        <p></p>
+        <p className="terms">
+          By clicking the button below I accept Cleanapp&#39;s
+          <br /><Link className="terms-link" to="/">Terms of Use</Link>
+        </p>
         <Link className="bottom-button" to="finish">Make Payment</Link>
       </div>
 
